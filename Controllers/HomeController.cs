e5b0273d34cd5977f19deb3b02e7b2419fb1d69e
satishjpr <sathishhn2005@outlook.com>
2022-10-15 23:04:11 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FloatingPillars.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult PropertiesDetails()
+        {
+            ViewBag.Message = "Your PropertiesDetails page.";
+
+            return View();
+        }
+        public ActionResult PropertiesRightSidebar()
+        {
+            ViewBag.Message = "Your PropertiesRightSidebar page.";
+
+            return View();
+        }
+    }
+}
